0179931fe43ba2d563ceeedd32b8f864c37eb4d0
miihfranca <michelefdf@gmail.com>
2021-11-20 23:09:42 -0300

    atividade projeto final

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './components/footer/Footer.jsx';
+import Header from './components/header/Header.jsx';
+
+function App() {
+  return (
+    <main className="App">
+      <Header/>
+      <Footer/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import '../footer/Footer.css'
+
+export default function Footer(){
+    return(
+        <div className='container'>
+            <footer className='footer'>
+            </footer>
+        </div>
+    );
+}
