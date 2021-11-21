2eeaf7d3b5028882eabcf086d329b1d9122b50ca
miihfranca <michelefdf@gmail.com>
2021-11-20 23:35:05 -0300

    projeto final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Footer from './components/footer/Footer.jsx';
-import Header from './components/header/Header.jsx';
+
 
 function App() {
   return (
     <main className="App">
-      <Header/>
-      <Footer/>
+     
     </main>
   );
 }
